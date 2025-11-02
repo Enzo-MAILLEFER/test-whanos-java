5248b0df57c2cb68604dc56a63b6d9ec7a3191f6
Enzo-MAILLEFER <enzo.maillefer@epitech.eu>
2025-11-02 22:19:09 +0100

    feat: add server java

--- app/src/main/java/eu/epitech/App.java
+++ app/src/main/java/eu/epitech/App.java
@@ -1,13 +1,19 @@
 package eu.epitech;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            byte[] resp = "Hello World!".getBytes();
+            exchange.sendResponseHeaders(200, resp.length);
+            exchange.getResponseBody().write(resp);
+            exchange.close();
+        });
+        server.setExecutor(null);
+        System.out.println("Server started on port 8080");
+        server.start();
     }
 }
